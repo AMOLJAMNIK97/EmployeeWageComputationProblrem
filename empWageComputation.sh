cccf3a6b0028891b5fcab42223e4d1a466de6d4f
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-11-24 09:12:30 +0530

    Calculating Wages for a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,17 +1,35 @@
-isPartTime=4;
-isfullTime=8;
-empRatePerHr=20;
-empCheck=$((RANDOM%3));
+perHourSalary=20;
+workingHour=0;
+totalSalary=0;
+totalWorkingHour=0;
+day=1;
+while [[ $day -le 20 && $totalWorkingHour -lt 40 ]]
+do
+	isPresent=$((RANDOM%3));
+	case $isPresent in
+		0)
+		#echo "Employee is absent";
+		workingHour=0;
+		;;
 
-case $empCheck in
-	$isfullTime)
-		empHrs=8;
+		1)
+		#echo "Employee is present";
+		workingHour=8;
 		;;
-	$ispartTime)
-		empHrs=4;
+
+		2)
+		#echo "Employee is working as part time";
+		workingHour=4;
 		;;
-	*)
-	empHrs=0
-		;;
-esac
-salary=$(($empHrs*$empRatePerHr));
+	esac
+	totalWorkingHour=$(($totalWorkingHour + $workingHour));
+	if [ $totalWorkingHour -gt 40 ]
+	then
+		totalWorkingHour=$(($totalWorkingHour - $workingHour));
+		break;
+	fi
+	salary=$(($perHourSalary * $workingHour));
+	totalSalary=$(($totalSalary + $salary));
+	((day++));
+done
+echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
